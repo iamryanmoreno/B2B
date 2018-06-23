c02e7400d785a4b468d97924c81e6cec66d4f1c1
Ryan <iamryanmoreno@gmail.com>
2018-06-23 23:02:57 +0800

    introduction and basics

---
+++ 01Basics/nullandbool.js
@@ -0,0 +1,13 @@
+var temp
+console.log("Current temperature is: "+temp)
+
+//grade system
+// 0 is not equal to undefined
+//boolean hold values either true or false
+let grade = true
+
+let actualMarks = 7
+let myGrade = actualMarks > 10
+console.log(myGrade)
+
+console.log(grade)
